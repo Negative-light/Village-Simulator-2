ea032809dfa4f4cc995fa81dc904235782f0771a
Negative-light <negalight414@gmail.com>
2021-05-18 16:05:16 -0400

    Made a Good Start on the V Sim

---
+++ VilSim.java
@@ -0,0 +1,8 @@
+public class VilSim {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO Village Simulator 2");
+        System.out.println("Writen by Negative_Light");
+
+    }
+
+}
---
+++ Village.java
@@ -0,0 +1,14 @@
+/**
+ * # Class: Village
+ * A Village
+ * ## EDIT LOG
+ * |Person|DATE|REASON|
+ * |-|-|
+ * |Negative_Light|5/18/21|Initial Creation|
+ */
+public class Village 
+{
+    //TODO: Make Villager Class
+    //TODO: Make Building Class
+    //TODO: PRODUCT Class
+}
